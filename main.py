51a06614d10f8ad9135e2be1066fe97be32045a4
huzaifa8883 <aw3540341@gmail.com>
2025-09-10 15:42:52 +0500

    Add main.py for training entry point

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from sklearn.model_selection import train_test_split
 from src.train import train_nn
 
 # Dataset
-x, y = make_moons(n_samples=1000, noise=0.2, random_state=42)
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+X, y = make_moons(n_samples=1000, noise=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 X_train, X_test = X_train.T, X_test.T
 y_train, y_test = y_train.reshape(1, -1).astype(float), y_test.reshape(1, -1).astype(float)
 
